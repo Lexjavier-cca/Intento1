04923f42ef330271279ff4d6f951c5d3057823a5
Alex-Caiza <ajcaizac1@uce.edu.ec>
2026-04-08 17:33:54 -0500

    Elimine el txt y añadi una carpeta Clases con una clase SumaJava

---
+++ Clases/SumaJava.java
@@ -0,0 +1,9 @@
+public class SumaJava {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int suma = num1 + num2;
+        System.out.println("La suma de " + num1 + " y " + num2 + " es: " + suma);
+    }
+
+}
